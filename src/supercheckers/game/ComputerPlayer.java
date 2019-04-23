8d5db398e4d8fda7ac6cc9d2aead711075eb18af
HamzaAli1 <hamzaimran2011@gmail.com>
2019-04-23 09:12:46 -0500

    haha ai

--- src/supercheckers/game/ComputerPlayer.java
+++ src/supercheckers/game/ComputerPlayer.java
@@ -36,57 +36,14 @@ public final class ComputerPlayer extends Player {
     }
     
     /**
-     * Logic:
-     * for (every piece computer has) {
-     *      if (any of its moves can kill a piece)
-     *           make that move (pick randomly if there are multiple)
-     *      return;
-     * }
-     * make a random move
+     * move method for com player. logic differs based on difficulty
      * @param g instance of the Game
      */
     @Override
     void move(Game g) {
         System.out.println("Computer is making its move...");
         
-        Piece p;
-        String moveL, moveR, moveL2, moveR2;
-        
-        killMoves(g);
-        
-        if (!kills.isEmpty()) {
-            p = kills.get((int) (kills.size()*Math.random()));
-            moveL = "" + (p.getRow()-2) + "," + (p.getColumn()-2);
-            moveR = "" + (p.getRow()-2) + "," + (p.getColumn()+2);
-            moveL2 = "" + (p.getRow()+2) + "," + (p.getColumn()-2);
-            moveR2 = "" + (p.getRow()+2) + "," + (p.getColumn()+2);
-
-            if (validMove(g, moveL.split("[,]"), p))
-                p.move(moveL.split("[,]"), g);
-            else if (validMove(g, moveR.split("[,]"), p))
-                p.move(moveR.split("[,]"), g);
-            else if (validMove(g, moveL2.split("[,]"), p))
-                p.move(moveL2.split("[,]"), g);
-            else
-                p.move(moveR2.split("[,]"), g);
-        }
-        else {
-            p = getRandPiece(g);
-            moveL = "" + (p.getRow()-1) + "," + (p.getColumn()-1);
-            moveR = "" + (p.getRow()-1) + "," + (p.getColumn()+1);
-            moveL2 = "" + (p.getRow()+1) + "," + (p.getColumn()-1);
-            moveR2 = "" + (p.getRow()+1) + "," + (p.getColumn()+1);
-            if (validMove(g, moveL.split("[,]"), p))
-                p.move(moveL.split("[,]"), g);
-            else if (validMove(g, moveR.split("[,]"), p))
-                p.move(moveR.split("[,]"), g);
-            else if (validMove(g, moveL2.split("[,]"), p))
-                p.move(moveL2.split("[,]"), g);
-            else if (validMove(g, moveR2.split("[,]"), p))
-                p.move(moveR2.split("[,]"), g);
-        }
-        
-        clearKillMoves();
+        easy(g);
         
         System.out.println("Computer has made a move.");
     }
@@ -132,6 +89,22 @@ public final class ComputerPlayer extends Player {
         panel.setGameOutput("Computer is making its move...");
         Thread.sleep(delay);
         
+        easy(g);
+        
+        Thread.sleep(delay);
+    }
+    
+    /**
+     * Logic:
+     * if (any of its moves can kill a piece) {
+     *      make that move (pick randomly if there are multiple)
+     * }
+     * else {
+     *      make a random move
+     * }
+     * @param g instance of the Game
+     */
+    private void easy(Game g) {
         Piece p;
         String moveL, moveR, moveL2, moveR2;
         
@@ -170,7 +143,128 @@ public final class ComputerPlayer extends Player {
         }
         
         clearKillMoves();
+    }
+    
+    /**
+     * Logic:
+     * if (any of its moves can kill a piece) {
+     *      make that move (pick move that kills the most, or is the safest move)
+     * }
+     * else {
+     *  > swap with opponent (let them kill so you can kill), only if not even or you are winning
+     *  > make a move that moves you into an edge spot (safe from capture)
+     *  > make a safe move?
+     *  > random move
+     * }
+     * @param g 
+     */
+    private void hard(Game g) {
+        Piece p;
+        String moveL, moveR, moveL2, moveR2;
         
-        Thread.sleep(delay);
+        killMoves(g);
+        
+        if (!kills.isEmpty()) { //add something for multiple hops
+            p = kills.get((int) (kills.size()*Math.random()));
+            moveL = "" + (p.getRow()-2) + "," + (p.getColumn()-2);
+            moveR = "" + (p.getRow()-2) + "," + (p.getColumn()+2);
+            moveL2 = "" + (p.getRow()+2) + "," + (p.getColumn()-2);
+            moveR2 = "" + (p.getRow()+2) + "," + (p.getColumn()+2);
+
+            if (validMove(g, moveL.split("[,]"), p))
+                p.move(moveL.split("[,]"), g);
+            else if (validMove(g, moveR.split("[,]"), p))
+                p.move(moveR.split("[,]"), g);
+            else if (validMove(g, moveL2.split("[,]"), p))
+                p.move(moveL2.split("[,]"), g);
+            else
+                p.move(moveR2.split("[,]"), g);
+        }
+        else {
+            if (this.getColor().equals("red")) {
+                if (g.getBoard().getReds().size() > g.getBoard().getBlacks().size()) {
+                    if (swapKills(g))
+                        return;
+                }
+                else if () {
+                    
+                }
+                
+            } else if (this.getColor().equals("black")) {
+                if (g.getBoard().getReds().size() < g.getBoard().getBlacks().size()) {
+                    if (swapKills(g))
+                        return;
+                }
+                
+            }
+        }
+        
+        clearKillMoves();
+    }
+    
+    private boolean swapKills(Game g) {
+        ArrayList<Piece> temp = new ArrayList<>();
+        Piece piece;
+        String move;
+        if (this.getColor().equals("red")) {
+            for (Piece r : g.getBoard().getReds()) {
+                if (this.pieceCanMove(g, r, this.getColor()))
+                    temp.add(r);
+            }
+            while (temp.size() > 0) {
+                piece = temp.remove((int)(temp.size() * Math.random()));
+                if (g.getBoard().pieceAtPoint(piece.getRow() - 2, piece.getColumn() + 2, "black") != null && g.getBoard().pieceAtPoint(piece.getRow() + 1, piece.getColumn() - 1, "red") != null) {
+                    move = "" + (piece.getRow()-1) + "," + (piece.getColumn()+1);
+                    if (validMove(g, move.split("[,]"), piece)) {
+                        piece.move(move.split("[,]"), g);
+                        return true;
+                    }
+                } else if (g.getBoard().pieceAtPoint(piece.getRow() - 2, piece.getColumn() - 2, "black") != null && g.getBoard().pieceAtPoint(piece.getRow() + 1, piece.getColumn() + 1, "red") != null) {
+                    move = "" + (piece.getRow()-1) + "," + (piece.getColumn()-1);
+                    if (validMove(g, move.split("[,]"), piece)) {
+                        piece.move(move.split("[,]"), g);
+                        return true;
+                    }
+                }
+            }
+        } else {
+            for (Piece b : g.getBoard().getBlacks()) {
+                if (this.pieceCanMove(g, b, this.getColor()))
+                    temp.add(b);
+            }
+            while (temp.size() > 0) {
+                piece = temp.remove((int)(temp.size() * Math.random()));
+                if (g.getBoard().pieceAtPoint(piece.getRow() + 2, piece.getColumn() + 2, "red") != null && g.getBoard().pieceAtPoint(piece.getRow() - 1, piece.getColumn() - 1, "black") != null) {
+                    move = "" + (piece.getRow()-1) + "," + (piece.getColumn()+1);
+                    if (validMove(g, move.split("[,]"), piece)) {
+                        piece.move(move.split("[,]"), g);
+                        return true;
+                    }
+                } else if (g.getBoard().pieceAtPoint(piece.getRow() + 2, piece.getColumn() - 2, "red") != null && g.getBoard().pieceAtPoint(piece.getRow() - 1, piece.getColumn() + 1, "black") != null) {
+                    move = "" + (piece.getRow()-1) + "," + (piece.getColumn()-1);
+                    if (validMove(g, move.split("[,]"), piece)) {
+                        piece.move(move.split("[,]"), g);
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    
+    private boolean edgeMove(Game g) {
+        ArrayList<Piece> temp = new ArrayList<>();
+        Piece piece;
+        String move;
+        if (this.getColor().equals("red")) {
+            for (Piece r : g.getBoard().getReds()) {
+                if (this.pieceCanMove(g, r, this.getColor()) && (r.getColumn() == 1 || r.getColumn() == 6));
+                    temp.add(r);
+            }
+            while (temp.size() > 0) {
+                piece = temp.remove((int)(temp.size() * Math.random()));
+                if ()
+            }
+        }
     }
 }
